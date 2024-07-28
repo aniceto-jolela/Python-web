310c6c6a7af7e66b544c88c743c9b8c84a1ec486
Aniceto Jolela <79465134+aniceto-jolela@users.noreply.github.com>
2024-07-28 14:30:52 +0000

    create template.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Aniceto',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'José',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    },
+    {
+        'author': 'Miranda',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'August 29, 2018'
+    },
+]
+
 def home(request):
-  return HttpResponse("<h1>Hello, world.</h1>")
+  context = {
+    'posts':posts
+  }
+  return render(request,'blog/home.html',context)
+
+def about(request):
+  return render(request,'blog/about.html', {'title': 'About'})
